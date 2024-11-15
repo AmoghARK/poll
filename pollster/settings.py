563e90bce60b813ebfbda5700140296db78f0f43
Amogh Keluskar <amoghkelu@gmail.com>
2024-11-15 16:39:09 +0530

    push2

--- pollster/settings.py
+++ pollster/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '1^_&3^e5&d&^1r)7q21ku7kif0czi07fiw_1^&ref^k!y#_h-d'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [ 'pollster-unba.onrender.com' ]
 
-# Application definition
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
